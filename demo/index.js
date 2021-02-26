6434c7a4c118ed5a3c76a9a95a5fdcc86826627b
Max <mdeng@bellus3d.com>
2021-02-25 23:55:40 -0800

    initial commit

---
+++ demo/index.js
@@ -0,0 +1,361 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as facemesh from "@tensorflow-models/facemesh";
+import Stats from "stats.js";
+import * as tf from "@tensorflow/tfjs-core";
+import * as tfjsWasm from "@tensorflow/tfjs-backend-wasm";
+// TODO(annxingyuan): read version from tfjsWasm directly once
+// https://github.com/tensorflow/tfjs/pull/2819 is merged.
+import { version } from "@tensorflow/tfjs-backend-wasm/dist/version";
+
+import { TRIANGULATION } from "./triangulation";
+
+tfjsWasm.setWasmPath(
+  `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${version}/dist/tfjs-backend-wasm.wasm`
+);
+
+function isMobile() {
+  const isAndroid = /Android/i.test(navigator.userAgent);
+  const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  return isAndroid || isiOS;
+}
+
+function drawPath(ctx, points, closePath) {
+  const region = new Path2D();
+  region.moveTo(points[0][0], points[0][1]);
+  for (let i = 1; i < points.length; i++) {
+    const point = points[i];
+    region.lineTo(point[0], point[1]);
+  }
+
+  if (closePath) {
+    region.closePath();
+  }
+  ctx.stroke(region);
+}
+
+let model,
+  ctx,
+  videoWidth,
+  videoHeight,
+  video,
+  canvas,
+  scatterGLHasInitialized = false,
+  scatterGL;
+
+const VIDEO_SIZE = 400;
+const mobile = isMobile();
+// Don't render the point cloud on mobile in order to maximize performance and
+// to avoid crowding limited screen space.
+const renderPointcloud = mobile === false;
+const stats = new Stats();
+const state = {
+  backend: "wasm",
+  maxFaces: 1,
+  triangulateMesh: true,
+};
+
+if (renderPointcloud) {
+  state.renderPointcloud = true;
+}
+
+// to capture next frame
+window.captureNextFrame = false;
+window.capturePredictionData = {};
+window.capturedFrameCount = 0;
+function cloneCanvas(oldCanvas, newCanvas) {
+  //create a new canvas
+  var context = newCanvas.getContext("2d");
+  context.clearRect(0, 0, newCanvas.width, newCanvas.height);
+
+  //set dimensions
+  newCanvas.width = oldCanvas.width;
+  newCanvas.height = oldCanvas.height;
+
+  //apply the old canvas to the new one
+  context.drawImage(oldCanvas, 0, 0);
+}
+
+function setupDatGui() {
+  const gui = new dat.GUI();
+  gui
+    .add(state, "backend", ["wasm", "webgl", "cpu"])
+    .onChange(async (backend) => {
+      await tf.setBackend(backend);
+    });
+
+  gui.add(state, "maxFaces", 1, 20, 1).onChange(async (val) => {
+    model = await facemesh.load({ maxFaces: val });
+  });
+
+  gui.add(state, "triangulateMesh");
+
+  if (renderPointcloud) {
+    gui.add(state, "renderPointcloud").onChange((render) => {
+      document.querySelector("#scatter-gl-container").style.display = render
+        ? "inline-block"
+        : "none";
+    });
+  }
+}
+
+async function setupCamera() {
+  video = document.getElementById("video");
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      facingMode: "user",
+      // Only setting the video to a specified size in order to accommodate a
+      // point cloud, so on mobile devices accept the default size.
+      width: mobile ? undefined : VIDEO_SIZE,
+      height: mobile ? undefined : VIDEO_SIZE,
+    },
+  });
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+
+
+
+function centroidize(mat) {
+  let c = [0, 0, 0];
+  try {
+    // calculate
+    mat.forEach((v) => {
+      c[0] += v[0];
+      c[1] += v[1];
+      c[2] += v[2];
+    });
+    c[0] /= mat.length;
+    c[1] /= mat.length;
+    c[2] /= mat.length;
+    // move to center
+    let newMat = [];
+    mat.forEach((v) => {
+      let newVector = v.slice();
+      newVector[0] -= c[0];
+      newVector[1] -= c[1];
+      newVector[2] -= c[2];
+      newMat.push(newVector);
+    });
+    console.log("centroid: ", c);
+    return newMat;
+  } catch (e) {
+    return mat;
+  }
+}
+
+function matrixAdd(A, B) {
+  var result = new Array(A.length)
+    .fill(0)
+    .map((row) => new Array(A[0].length).fill(0));
+
+  return result.map((row, i) => {
+    return row.map((val, j) => {
+      return A[i][j] + B[i][j];
+    });
+  });
+}
+
+
+async function renderPrediction() {
+  stats.begin();
+
+  const predictions = await model.estimateFaces(video);
+
+  ctx.drawImage(
+    video,
+    0,
+    0,
+    videoWidth,
+    videoHeight,
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  let f = [];
+
+  if (predictions.length > 0) {
+
+    predictions.forEach((prediction) => {
+      const keypoints = prediction.scaledMesh;
+
+      let ff = [];
+
+      if (state.triangulateMesh) {
+        for (let i = 0; i < TRIANGULATION.length / 3; i++) {
+          const points = [
+            TRIANGULATION[i * 3],
+            TRIANGULATION[i * 3 + 1],
+            TRIANGULATION[i * 3 + 2],
+          ].map((index) => keypoints[index]);
+
+          if(window.captureNextFrame){
+            ff.push([
+              TRIANGULATION[i * 3],
+              TRIANGULATION[i * 3 + 1],
+              TRIANGULATION[i * 3 + 2],
+            ]);
+          }
+
+          drawPath(ctx, points, true);
+        }
+
+        f.push(ff);
+
+      } else {
+        for (let i = 0; i < keypoints.length; i++) {
+          const x = keypoints[i][0];
+          const y = keypoints[i][1];
+
+          ctx.beginPath();
+          ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
+          ctx.fill();
+        }
+      }
+    });
+
+    if (renderPointcloud && state.renderPointcloud && scatterGL != null) {
+      const pointsData = predictions.map((prediction) => {
+        let scaledMesh = prediction.scaledMesh;
+        return scaledMesh.map((point) => [-point[0], -point[1], -point[2]]);
+      });
+
+      let flattenedPointsData = [];
+      for (let i = 0; i < pointsData.length; i++) {
+        flattenedPointsData = flattenedPointsData.concat(pointsData[i]);
+      }
+      const dataset = new ScatterGL.Dataset(flattenedPointsData);
+
+      if (!scatterGLHasInitialized) {
+        scatterGL.render(dataset);
+      } else {
+        scatterGL.updateDataset(dataset);
+      }
+      scatterGLHasInitialized = true;
+    }
+  }
+
+  // check if we capture this frame
+  if (window.captureNextFrame) {
+    window.captureNextFrame = false;
+    // clone canvas
+    // capture content
+    try {
+      let output = canvas;
+      let scatterGL = document.querySelector("#scatter-gl-container canvas");
+      let captureFrame = document.querySelector("#capture-frame");
+      let captureScatterGL = document.querySelector("#capture-scatter-gl");
+      cloneCanvas(output, captureFrame);
+      cloneCanvas(scatterGL, captureScatterGL);
+    } catch (e) {
+      alert("capture failed, error: ", e.message);
+    }
+
+    let capturePredictions = document.querySelector("#capture-predictions");
+    try {
+      let firstFace = predictions[0];
+      let { mesh, scaledMesh } = firstFace;
+
+      mesh = centroidize(mesh);
+      scaledMesh = centroidize(scaledMesh);
+
+      // accumulate prediction data 
+      // in this way, we can support both single capture and continuous capture
+      if(window.capturePredictionData.mesh && window.capturePredictionData.scaledMesh){
+        window.capturePredictionData.mesh = matrixAdd(window.capturePredictionData.mesh, mesh);
+        window.capturePredictionData.scaledMesh = matrixAdd(window.capturePredictionData.scaledMesh, scaledMesh);
+        window.capturePredictionData.f = f[0];
+      }else{
+        window.capturePredictionData = { originalJSON: firstFace, mesh, scaledMesh, f: f[0] };
+      }
+      window.capturedFrameCount += 1;
+
+      capturePredictions.innerHTML = JSON.stringify(
+        window.capturePredictionData,
+        null,
+        2
+      );
+      // log it to console for debugging
+      console.log(firstFace);
+    } catch (e) {
+      window.capturePredictionData = { error: { message: e.message } };
+      capturePredictions.innerHTML = JSON.stringify(
+        window.capturePredictionData,
+        null,
+        2
+      );
+    }
+  }
+
+  stats.end();
+  requestAnimationFrame(renderPrediction);
+}
+
+async function main() {
+  await tf.setBackend(state.backend);
+  setupDatGui();
+
+  stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.getElementById("main").appendChild(stats.dom);
+
+  await setupCamera();
+  video.play();
+  videoWidth = video.videoWidth;
+  videoHeight = video.videoHeight;
+  video.width = videoWidth;
+  video.height = videoHeight;
+
+  canvas = document.getElementById("output");
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+  const canvasContainer = document.querySelector(".canvas-wrapper");
+  canvasContainer.style = `width: ${videoWidth}px; height: ${videoHeight}px`;
+
+  ctx = canvas.getContext("2d");
+  ctx.translate(canvas.width, 0);
+  ctx.scale(-1, 1);
+  ctx.fillStyle = "#32EEDB";
+  ctx.strokeStyle = "#32EEDB";
+  ctx.lineWidth = 0.5;
+
+  model = await facemesh.load({ maxFaces: state.maxFaces });
+  renderPrediction();
+
+  if (renderPointcloud) {
+    document.querySelector(
+      "#scatter-gl-container"
+    ).style = `width: ${VIDEO_SIZE}px; height: ${VIDEO_SIZE}px;`;
+
+    scatterGL = new ScatterGL(document.querySelector("#scatter-gl-container"), {
+      rotateOnStart: false,
+      selectEnabled: false,
+    });
+  }
+}
+
+main();
